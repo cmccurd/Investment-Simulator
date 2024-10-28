5ecc34e2f05167dab10d6ae051df8037be5b7597
cmccurd <cam.alex.mccurdy@gmail.com>
2024-10-27 20:09:33 -0400

    removed old values to start new

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # Disable tracking for performance
